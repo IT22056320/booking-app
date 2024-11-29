e2c279491e2e6887b353a75dad2afee920fad501
UdulaThathsaridu <it22056320@my.sliit.lk>
2024-11-29 12:59:06 +0530

    added booking app

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const {Router} =require("express");
+const {getBookings, createBooking, updateBooking, deleteBooking, getBooking} = require("../controllers/bookingController");
+
+const router =Router();
+
+router.get("/",getBookings);
+router.post("/",createBooking);
+router.put("/:id",updateBooking);
+router.delete("/:id",deleteBooking);
+router.get("/:id",getBooking)
+
+module.exports = router;
